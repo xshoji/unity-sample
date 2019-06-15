410b4c04bdc627ed434021b49d2d27b3b0d925e7
xshoji <xshoji@users.noreply.github.com>
2019-06-16 02:25:20 +0900

    Add EndingScene

---
+++ Assets/Scripts/CloseButtonScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseButtonScript : MonoBehaviour
+{
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalScript : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //オブジェクトが触れている間
+    void OnTriggerEnter(Collider t)
+    {
+        if (t.gameObject == player)
+        {
+            SceneManager.LoadScene("EndingScene");
+        }
+    }
+}
