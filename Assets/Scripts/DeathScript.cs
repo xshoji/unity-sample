d6e8fc1ef3dd977b19d0f1315b7c9d93a3891a2a
xshoji <xshoji@users.noreply.github.com>
2019-06-16 01:18:47 +0900

    Add Death

---
+++ Assets/Scripts/DeathScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScript : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var tmp = player.transform.position;
+        if (tmp.y < -10)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
