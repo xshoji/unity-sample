9b1e3ccf3e5d597372f4cd3a4d47f645d674084f
xshoji <xshoji@users.noreply.github.com>
2019-06-16 03:00:27 +0900

    Add TitleScene

---
+++ Assets/Scripts/StartButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonScript : MonoBehaviour
+{
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
